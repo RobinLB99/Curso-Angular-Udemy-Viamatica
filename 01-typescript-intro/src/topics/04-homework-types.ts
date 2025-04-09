6337fc98d2f632f82a7b1fa537d14406590ec106
Robin Lugo Boero <lugoboero1999@outlook.com>
2025-04-09 14:45:33 -0500

    Tarea: interfaz de definición de tipo de objeto.

---
+++ 01-typescript-intro/src/topics/04-homework-types.ts
@@ -0,0 +1,35 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+/**
+ * Tarea: Crear la interfaz de definición de un objeto tipo SuperHeroe.
+ */
+interface SuperHero {
+  name: string;
+  age: number;
+  address: Address;
+  showAddress: () => string;
+}
+
+interface Address {
+  street: string;
+  country: string;
+  city: string;
+}
+
+const superHeroe: SuperHero = {
+  name: "Spiderman",
+  age: 30,
+  address: {
+    street: "Main St",
+    country: "USA",
+    city: "NY",
+  },
+  showAddress() {
+    return `${this.name}, ${this.address.city}, ${this.address.country}`;
+  },
+};
+
+const address = superHeroe.showAddress();
+console.log(address);
