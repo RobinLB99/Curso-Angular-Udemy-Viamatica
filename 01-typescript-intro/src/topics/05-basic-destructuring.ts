0bbf9eecbeca1fc82c17d82136c700e00e1988f7
Robin Lugo Boero <lugoboero1999@outlook.com>
2025-04-09 16:03:35 -0500

    Destructuracion de arreglos

--- 01-typescript-intro/src/topics/05-basic-destructuring.ts
+++ 01-typescript-intro/src/topics/05-basic-destructuring.ts
@@ -1,3 +1,7 @@
+/**
+ * Desestructuración de objetos.
+ */
+
 interface AudioPlayer {
   audioVolume: number;
   songDuration: number;
@@ -22,15 +26,30 @@ const audioPlayer: AudioPlayer = {
 
 const song = "New Song";
 
-// without destructuring
-console.log("Song: " + audioPlayer.song);
+/* without destructuring */
+/* console.log("Song: " + audioPlayer.song);
 console.log("Song Duration: " + audioPlayer.songDuration);
 console.log("Author: " + audioPlayer.details.author);
-
-// with destructuring
+ */
+/* with destructuring */
 const { song: otherSong, songDuration: duration, details } = audioPlayer; // se destructura la propiedad song y songDuration del objeto audioPlayer en unas constantes llamadas otherSong y duration. Sintaxis: 'propiedad: nombreDeLaContante'
 const { author } = details;
 
-console.log("Song: " + otherSong);
+/* console.log("Song: " + otherSong);
 console.log("Song Duration: " + duration);
 console.log("Author: " + author);
+ */
+
+/**
+ * Desestructuración de arreglos.
+ */
+const dbz: string[] = ["Goku", "Vegeta", "Trunks"];
+
+// without destructuring
+const trunks0 = dbz[2];
+
+console.log("Personaje no destructurado: " + trunks0);
+
+// with destructuring
+const [p1, p2, trunks1 = "not found"] = dbz; // se destructura el arreglo dbz en las constantes goku, vegeta y trunks2. Sintaxis: 'nombreDeLaContante: propiedad'
+console.log("Personaje destructurado: " + trunks1);
