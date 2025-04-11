970a1fa9c30694c05ffb16353fab0da6ab05fd9b
Robin Lugo Boero <lugoboero1999@outlook.com>
2025-04-11 15:17:49 -0500

    Se agrega comentarios de los conceptos de Signals

--- 02.1-repaso_aplicacion_bases_angular/src/app/pages/counter/counter.component.ts
+++ 02.1-repaso_aplicacion_bases_angular/src/app/pages/counter/counter.component.ts
@@ -7,6 +7,17 @@ import { Component, signal } from '@angular/core';
   styleUrl: './counter.component.css',
 })
 export class CounterComponent {
+  // Usando Angular Signals
+  // counterSignal es una señal que almacena el valor del contador
+  // y se inicializa en 0.
+  // La señal es una forma de manejar el estado reactivo en Angular.
+  // Se puede pensar en ella como una variable que notifica a los componentes
+  // cuando su valor cambia, lo que permite que los componentes se actualicen
+  // automáticamente cuando el valor de la señal cambia.
+  // En este caso, la señal se utiliza para almacenar el valor del contador
+  // y se actualiza cada vez que se incrementa o decrementa el contador.
+  // La señal se puede utilizar en la plantilla HTML para mostrar el valor
+  // del contador y se actualiza automáticamente cuando el valor de la señal cambia.
   counterSignal = signal(0);
 
   incrementIn(value: number): void {
