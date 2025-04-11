c25326dcc469742c76b937fcc4ccf0a7952c1815
Robin Lugo Boero <lugoboero1999@outlook.com>
2025-04-11 14:41:56 -0500

    Señales

--- 02-bases/src/app/pages/counter/counter.component.ts
+++ 02-bases/src/app/pages/counter/counter.component.ts
@@ -1,33 +1,30 @@
-import { Component } from '@angular/core';
-
-const templateHTML: string = `
-  <h1>Counter</h1>
-  <hr />
-  <h2>{{ counter }}</h2>
-  <button (click)="increaseBy(1)">+1</button>
-  <button (click)="decreaseBy(1)">-1</button>
-  <button (click)="resetCounter()">Reset</button>
-`;
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
   imports: [],
   templateUrl: './counter.component.html',
-  // template: templateHTML,
   styleUrl: './counter.component.css',
 })
 export class CounterComponent {
   counter: number = 0;
 
+  // Señales
+  counterSignal = signal(0); // Señal que se puede escribir.
+
   increaseBy(value: number): void {
     this.counter += value;
+    // this.counterSignal.set(this.counterSignal() + value); // Actualiza la señal. (no recomendado)
+    this.counterSignal.update((current) => current + value); // Actualiza la señal. (recomendado)
   }
 
   decreaseBy(value: number): void {
     this.counter -= value;
+    this.counterSignal.update((current) => current - value);
   }
 
   resetCounter(): void {
     this.counter = 0;
+    this.counterSignal.set(0);
   }
 }
