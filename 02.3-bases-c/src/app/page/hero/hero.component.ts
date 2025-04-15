c0bb12c989233cec42e1e4991e7e93722c7022b1
Robin Lugo Boero <lugoboero1999@outlook.com>
2025-04-15 15:06:48 -0500

    señales computadas

---
+++ 02.3-bases-c/src/app/page/hero/hero.component.ts
@@ -0,0 +1,44 @@
+import {
+  Component,
+  computed,
+  Signal,
+  signal,
+  WritableSignal,
+} from '@angular/core';
+
+@Component({
+  imports: [],
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.css',
+})
+export class HeroComponent {
+  nombre: WritableSignal<string> = signal('Ironman');
+  edad: WritableSignal<number> = signal(45);
+
+  // Señal computada. Cambia automáticamente cuando cambia el nombre o la edad
+  heroDescription: Signal<string> = computed(() => {
+    return `${this.nombre()} | Edad: ${this.edad()} años`;
+  });
+
+  capitalizedName: Signal<string> = computed(() => {
+    return this.nombre().toUpperCase();
+  });
+
+  capitalizado(): string {
+    return this.nombre().toUpperCase();
+  }
+
+  changeHero(): void {
+    this.nombre.set('Spiderman');
+    this.edad.set(25);
+  }
+
+  resetForm(): void {
+    this.nombre.set('Ironman');
+    this.edad.set(45);
+  }
+
+  changeAge() {
+    this.edad.set(60);
+  }
+}
